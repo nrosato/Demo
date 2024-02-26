455509372460efe12c53675e40640636a2ed00c2
nick <nick.rosato@hpe.com>
2024-02-26 15:40:14 -0600

    Setup project directories

---
+++ src/main/java/com/deloitte/demo/SpringBootcampDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcampDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcampDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deloitte/demo/SpringBootcampDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcampDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
